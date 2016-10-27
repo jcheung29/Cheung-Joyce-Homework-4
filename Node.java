a8b63fd1a780f93514e24c77c0febab9ca47a9ea
jcheung29 <jcheung@albany.edu>
2016-10-27 18:24:06 -0400

    node class
    Created a singly linked node class

---
+++ Node.java
@@ -0,0 +1,37 @@
+/**
+ * Joyce Cheung
+ * ICSI 213
+ * Dr. Magnus
+ * Assignment 3.2
+ **/
+
+public class Node {
+  private String data;
+  private Node next; // reference to successor 
+  
+  Node (String x) {
+    this.data = x;
+    this.next = null;
+  }
+  
+  //Insert getters and setters
+  public String getData() {
+  return this.data;
+  }
+  
+  public void setData(String newData) {
+  this.data = newData;
+  }
+  
+  public Node getNext() {
+  return this.next;
+  }
+  
+  public void setNext(Node newNext) {
+  this.next = newNext;
+  }
+  }
+
+
+  
+  
